b572d932d748e55aee8ad31b18d060024eb269f8
Farrukh Karimov <farrukhkarimov@Farrukhs-MacBook-Pro.local>
2022-02-10 15:00:03 -0500

    first react app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import FirstComponent from './FirstComponent';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
+        <FirstComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first React app.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bocacode.com"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -19,6 +22,8 @@ function App() {
         </a>
       </header>
     </div>
+    <p></p>
+    </>
   );
 }
 
